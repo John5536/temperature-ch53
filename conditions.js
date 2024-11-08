ad67d4b0319b08570f8ead50453ffae801294b9c
John5536 <rrivers52@gmail.com>
2024-11-08 16:32:22 -0500

    Assignment-3

--- conditions.js
+++ conditions.js
@@ -1,4 +1,4 @@
-console.log("if-statement")
+// console.log("if-statement")
 
 // if(condition){
 
@@ -18,23 +18,74 @@ console.log("if-statement")
 //     console.log("no");
 // }
 
-function the_weather(){
-    let temperature = prompt("enter the temperature in celcius");
+// function the_weather(){
+//     let temperature = prompt("enter the temperature in celcius");
     
-    if (temperature<15){
-        console.log("PLease wear a jacket");
-        document.getElementById("weather").classList.add("cold");
+//     if (temperature<15){
+//         console.log("PLease wear a jacket");
+//         document.getElementById("weather").classList.add("cold");
+//     }
+//     else if(temperature<25){
+//         console.log("please wear a sweater");
+//     }
+//     else if(temperature>=25){
+//         console.log("Your can wear a t-shirt");
+//     }
+//     else{
+//         console.log("null");
+//     }
+//     document.getElementById("weather").innerHTML=("cold");
+// }
+
+// the_weather()
+   
+function convertTemperature (){
+
+    
+    let temperature_choice= prompt("Would you like to enter temperature in Celcius or Fahrenheit ?");
+    let temperature_lower = temperature_choice.toLowerCase();
+
+    if (temperature_lower == "celcius"){
+        let celcius_input = prompt("input our temperature in to convert it to fahrenheit");
+        let conversionTocelcius = (celcius_input * 9/5) + 32;
+        alert("your temperature converted is " + conversionTocelcius);
+        if (conversionTocelcius< 15){
+            alert("Please wear a Jacket");
+            document.getElementById("weather").classList.add("cold");
+        }
+        else if (conversionTocelcius<25){
+            alert("Please wear a sweater");
+            document.getElementById("weather").classList.add("cold");
+        }
+        else if (conversionTocelcius>=25){
+            alert("Please wear a t shirt");
+            document.getElementById("weather").classList.add("hot");
+        }
+
     }
-    else if(temperature<25){
-        console.log("please wear a sweater");
+    else if (temperature_lower == "fahrenheit"){
+        let fahrenheit_input = prompt("input our temperature in to convert it to celcius");
+        let conversionTofahrenheit = (fahrenheit_input - 32) * 5/9;
+        alert("your temperature converted is " + conversionTofahrenheit);
     }
-    else if(temperature>=25){
-        console.log("Your can wear a t-shirt");
-    }
-    else{
-        console.log("null");
-    }
-    document.getElementById("weather").innerHTML=("cold");
+    
 }
 
-the_weather()
+// function Thermostat_controller(){
+
+//     if (conversionTocelcius< 15){
+//         alert("Please wear a Jacket");
+//         document.getElementById("weather").classList.add("cold");
+//     }
+//     else if (conversionTocelcius<25){
+//         alert("Please wear a sweater");
+//         document.getElementById("weather").classList.add("cold");
+//     }
+//     else if (conversionTocelcius>=25){
+//         alert("Please wear a t shirt");
+//         document.getElementById("weather").classList.add("hot");
+//     }
+// }
+
+convertTemperature ()
+//Thermostat_controller()
